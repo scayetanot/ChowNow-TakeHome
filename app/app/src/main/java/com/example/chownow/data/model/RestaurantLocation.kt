fdf2d33b3eed1d3191769bc798e3ba204dfc4659
Sebastien Cayetanot <sebastien.cayetanot@gmail.com>
2021-04-13 00:58:39 -0700

    Add name field in the Location Entity

--- app/app/src/main/java/com/example/chownow/data/model/RestaurantLocation.kt
+++ app/app/src/main/java/com/example/chownow/data/model/RestaurantLocation.kt
@@ -7,5 +7,7 @@ data class RestaurantLocation (
     @SerializedName("id")
     var id: String,
     @SerializedName("address")
-    var address: RestaurantAddress
+    var address: RestaurantAddress,
+    @SerializedName("name")
+    var name: String
 ) : Serializable
